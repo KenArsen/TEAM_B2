bbcd7a8e4633225c0e7a35989f560fa164711ba5
Baiel-vip <print.baiel@gmail.com>
2024-08-14 18:16:51 +0600

    2

--- practika.py
+++ practika.py
@@ -2,3 +2,10 @@ a = int(input('ведите длину'))
 b = int(input('ведите ширину'))
 c = a * b
 print(f'плошадь равен {c}')
+
+def baiel():
+    a, b = map(int,input().split())
+    s = a * b
+    print(s)
+baiel()
+
