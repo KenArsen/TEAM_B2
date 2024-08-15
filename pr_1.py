4ce2de62a643a70737cd61e75136578e8f849a3d
Baiel-vip <print.baiel@gmail.com>
2024-08-15 17:56:59 +0600

    iu

---
+++ pr_1.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f'Name: {self.name}')
+        print((f'Age: {self.age}'))
+
+pr = Animal('cat',4)
+
+pr.info()
